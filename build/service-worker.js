e0178e0a45220b4cc2944baf3f4ed9c9f1f0a597
matheus correa <mathec@live.com>
2021-08-27 12:27:06 -0300

    feat: release

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,59 +1,27 @@
-const CACHE_NAME = 'qrYParQ9xX';
+const CACHE_NAME = 'h5HDf4UKg8';
 const urlsToCache = [
-  '/',
-  '/assets/example_typings_2.png',
-  '/assets/example_typings.png',
-  '/icons/logo192.png',
-  '/icons/logo512.png',
-  '/index.html',
-  '/service-worker.js',
-  '/static/css/main.914f1c7f.chunk.css',
-  '/static/js/2.72a866fe.chunk.js',
-  '/static/js/3.e5c07b50.chunk.js',
-  '/static/js/4.32e7272d.chunk.js',
-  '/static/js/5.54080435.chunk.js',
-  '/static/js/main.c39963d6.chunk.js',
-  '/static/js/runtime-main.83b3c73e.js',
-  '/static/media/background.5b447c03.png',
+  "/",
+  "/assets/example_typings_2.png",
+  "/assets/example_typings.png",
+  "/icons/logo192.png",
+  "/icons/logo512.png",
+  "/index.html",
+  "/service-worker.js",
+  "/static/css/main.241160e1.chunk.css",
+  "/static/js/2.1750c387.chunk.js",
+  "/static/js/3.74ba8da4.chunk.js",
+  "/static/js/4.99ffa4d5.chunk.js",
+  "/static/js/5.3b90d0df.chunk.js",
+  "/static/js/main.34aea85e.chunk.js",
+  "/static/js/runtime-main.99cd0b4f.js",
+  "/static/media/background.5b447c03.png"
 ];
-
-// const BlackList = [
-//   'socket',
-// ];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[serviceWorker] add all cache from array');
-        return cache.addAll(urlsToCache);
-      }),
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  // ativa steps
-  console.log('[serviceWorker] ativo');
-  event.waitUntil(
-    // registrar o push
-    //  self.registration.pushManager.subscribe(SubscriptionOptions),
-    // limpar versão antiga
-    caches.keys().then((cacheNames) => Promise.all(cacheNames.map((thisCacheName) => {
-      if (thisCacheName !== CACHE_NAME) {
-        console.log('[Service Worker] Removing caching files from', thisCacheName);
-        return caches.delete(thisCacheName);
-      }
-
-      return false;
-    }))),
-
-  );
-});
-
 // const appIcon = '/icons/favicon.ico';
 
 // array routers on fetch not will cache
-
+const BlackList = [
+  'socket',
+];
 // https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications#request_permission
 
 // const urlB64ToUint8Array = (base64String) => {
@@ -74,31 +42,61 @@ self.addEventListener('activate', (event) => {
 //   userVisibleOnly: true,
 // };
 
-/**
- * cache all fetch data make inside app, this is not really usefull in some cases
- */
+this.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[serviceWorker] add all cache from array');
+        return cache.addAll(urlsToCache);
+      }),
+  );
+});
+
+this.addEventListener('activate', (event) => {
+  // ativa steps
+  console.log('[serviceWorker] ativo');
+  event.waitUntil(
+    // registrar o push
+    //  this.registration.pushManager.subscribe(SubscriptionOptions),
+    // limpar versão antiga
+    caches.keys().then((cacheNames) => Promise.all(cacheNames.map((thisCacheName) => {
+      if (thisCacheName !== CACHE_NAME) {
+        console.log('[Service Worker] Removing caching files from', thisCacheName);
+        return caches.delete(thisCacheName);
+      }
+
+      return false;
+    }))),
+
+  );
+});
+
 this.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request)),
   );
-  // e.respondWith((async () => {
-  //   const r = await caches.match(e.request);
-  //   if (r) { return r; }
-
-  //   return e.request;
-  // const response = await fetch(e.request);
-  // posts, put, delete are not allowed to cache
-  // if (e.request.method === 'GET' && !BlackList.includes(e.request.url)) {
-  //   console.log(`[Service Worker ${e.request.method}] Caching new resource from ${CACHE_NAME}: ${e.request.url}`);
-  //   const cache = await caches.open(CACHE_NAME);
-  //   cache.put(e.request, response.clone());
-  // }
-
-  // return response;
-  // })());
 });
 
-//  self.addEventListener("push", function (event) {
+/**
+   * cache all fetch data make inside app, this is not really usefull in some cases
+   */
+// this.addEventListener('fetch', (e) => {
+//   e.respondWith((async () => {
+//     const r = await caches.match(e.request);
+//     if (r) { return r; }
+//     const response = await fetch(e.request);
+//     // posts, put, delete are not allowed to cache
+//     if (e.request.method === 'GET' && !BlackList.includes(e.request.url)) {
+//       console.log(`[Service Worker ${e.request.method}] Caching new resource from ${CACHE_NAME}: ${e.request.url}`);
+//       const cache = await caches.open(CACHE_NAME);
+//       cache.put(e.request, response.clone());
+//     }
+
+//     return response;
+//   })());
+// });
+
+//  this.addEventListener("push", function (event) {
 //    /**@type {{body: string, title: string, icon: string?}} message */
 //    const message = event.data.json();
 //    console.log(message);
@@ -121,11 +119,11 @@ this.addEventListener('fetch', (event) => {
 //      };
 
 //      event.waitUntil(
-//        self.registration.showNotification(message.title, options)
+//        this.registration.showNotification(message.title, options)
 //      );
 //  });
 
-//  self.addEventListener("notificationclick", function openPushNotification(event) {
+//  this.addEventListener("notificationclick", function openPushNotification(event) {
 //      console.log("Notification click Received.", event.notification.data);
 //     event.notification.close();
 //    //do something
